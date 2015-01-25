f8e7d97dfa0d6b161db4204f97d1c7a6eb4fc1e9
Ben Sloboda <ben_sloboda@hotmail.com>
2015-01-24 21:02:25 -0700

    BDS - Movie attempt

---
+++ Assets/Scripts/PlayIntro.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayIntro : MonoBehaviour {
+	public MovieTexture movTexture;
+	void Start() {
+		renderer.material.mainTexture = movTexture;
+		movTexture.Play();
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
